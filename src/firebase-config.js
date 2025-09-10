493652343515f3a57cad130932998f1bdb057220
unknown <206084850+igtseth@users.noreply.github.com>
2025-09-10 16:50:25 +0300

    Add files via upload
    Signed-off-by: unknown <206084850+igtseth@users.noreply.github.com>

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// IMPORTANT: replace the values below with your Firebase project config
+// You can find this in Firebase Console -> Project Settings -> SDK setup and config
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
