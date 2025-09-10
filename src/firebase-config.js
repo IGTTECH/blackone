09f2b4b26f89c35a40e32f12ca80e2e3fdc3c535
unknown <206084850+igtseth@users.noreply.github.com>
2025-09-10 17:05:21 +0300

    Update firebase-config.js
    Signed-off-by: unknown <206084850+igtseth@users.noreply.github.com>

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,10 @@
 // IMPORTANT: replace the values below with your Firebase project config
 // You can find this in Firebase Console -> Project Settings -> SDK setup and config
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCFn1V2FRpwXqz1JQHerG9PuQ2GY4ggEzw",
+    authDomain: "famillyweb-bf64c.firebaseapp.com",
+    projectId: "famillyweb-bf64c",
+    storageBucket: "famillyweb-bf64c.firebasestorage.app",
+    messagingSenderId: "914238498025",
+    appId: "1:914238498025:web:873838c0b017f6d0f1fc78"
 };
