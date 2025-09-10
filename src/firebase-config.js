b22044f7337c32492dc19016a79e41bd4edb8d85
unknown <206084850+igtseth@users.noreply.github.com>
2025-09-10 17:13:08 +0300

    Update firebase-config.js
    Signed-off-by: unknown <206084850+igtseth@users.noreply.github.com>

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,10 @@
 // IMPORTANT: replace the values below with your Firebase project config
 // You can find this in Firebase Console -> Project Settings -> SDK setup and config
 export const firebaseConfig = {
-  apiKey: "AIzaSyCFn1V2FRpwXqz1JQHerG9PuQ2GY4ggEzw",
-    authDomain: "famillyweb-bf64c.firebaseapp.com",
-    projectId: "famillyweb-bf64c",
-    storageBucket: "famillyweb-bf64c.firebasestorage.app",
-    messagingSenderId: "914238498025",
-    appId: "1:914238498025:web:873838c0b017f6d0f1fc78"
+  apiKey: "AIzaSyBGBL3C-zTw6vwvIhktUE6mFfanLa-emJA",
+    authDomain: "igt-project-12443.firebaseapp.com",
+    projectId: "igt-project-12443",
+    storageBucket: "igt-project-12443.firebasestorage.app",
+    messagingSenderId: "458543900333",
+    appId: "1:458543900333:web:5781a612406ef2dfe3c824"
 };
